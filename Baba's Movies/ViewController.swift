39a50c84ce03215466f1f4c9514820e857763f5d
Walter Chiwo <waltchiwo@gmail.com>
2022-12-27 16:06:06 -0600

    Initial Commit

---
+++ Baba's Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baba's Movies
+//
+//  Created by Walter Chiwo on 12/27/22.
+//  Copyright © 2022 Walter Chiwo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
