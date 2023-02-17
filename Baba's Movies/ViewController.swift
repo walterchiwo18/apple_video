e7666e6db334b79123cf28a16955cece1ab5176b
Walter Chiwo <waltchiwo@gmail.com>
2023-02-17 14:45:04 -0600

    Add AV foundation and kit library in build phase and import libraries in view controller

--- Baba's Movies/ViewController.swift
+++ Baba's Movies/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    
 
 }
 
